1ea3a96e1c1b52a6fb206cce628022d0b9d21fd9
Ondkloss <ondkloss@gmail.com>
2019-05-08 16:21:14 +0200

    Added build.gradle and restructured source and test to accomodate. Slightly updated README.

--- src/test/java/dev/strand/netsgiro/parser/ParserTest.java
+++ src/test/java/dev/strand/netsgiro/parser/ParserTest.java
@@ -5,6 +5,7 @@ import dev.strand.netsgiro.exception.ParseException;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -13,8 +14,9 @@ import java.util.List;
 public class ParserTest {
 
     @Test
-    public void parserTest() throws ParseException, IOException {
-        List<String> lines = Files.readAllLines(Paths.get("test","res", "ocr1.txt"), StandardCharsets.UTF_8);
+    public void parserTest() throws ParseException, IOException, URISyntaxException {
+        List<String> lines = Files.readAllLines(Paths.get(this.getClass().getResource("/ocr1.txt").toURI()),
+                StandardCharsets.UTF_8);
         Parser p = new Parser(lines.toArray(new String[0]));
         Forsendelse f = p.parse();
         System.out.println(f);
